4fca6ee07ebabeb362e5db6471341e012a7beab9
JustAaron <arronlee17@gmail.com>
2018-08-11 13:41:48 -0400

    basic methods
    basic methods and variables created

--- game/resources/views/play.blade.php
+++ game/resources/views/play.blade.php
@@ -8,12 +8,77 @@
     </div>
     <script>
         var $container = document.getElementById('container')
-
+        var numberCards = 6;
+        var numberPlayers = 4;
+        var cardsPlayed = [];
+        var playerHand = [];
+        var cardsInDeck = [];
+        var playersTurn = 0;
+        var cardsOnBoard = [];
+        var clearedCards = [];
+        var passingEnabled = true;
+        var playerPlayedCards = [];
         // Instantiate a deck
         var deck = Deck();
 
         // display it in a html container
         var $container = document.getElementById('container');
         deck.mount($container);
+
+        function deal(){
+
+        }
+
+        function attack(){
+        	if(isPlayersTurn()){
+
+        	}
+
+        }
+
+        function defend(){
+        	if(isPlayersTurn()){
+
+        	}
+
+        }
+
+        function forfeitTurn{
+        	if(isPlayersTurn()){
+
+        	}
+
+        }
+
+        function pass(){
+        	if(isPlayersTurn()){
+
+        	}
+        }
+
+        function goNextPlayer(){
+        	if(playersTurn < numberPlayers){
+        		playersTurn ++;
+        	}
+        	else{
+        		playersTurn = 0;
+        	}
+        }
+
+        function isPlayersTurn(){
+        	if(playersTurn === 0){
+        		return true;
+        	}
+        	return false;
+        }
+
+        function playCards(){
+        	playerPlayedCards.push
+        }
+
+        function main(){
+
+        }
+        main();
     </script>
 @stop
