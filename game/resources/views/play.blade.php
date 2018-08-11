a7a9ab99d406b4363b4df730cfe599ee8cb02dc4
DianaSoltani <diana.soltani@rutgers.edu>
2018-08-11 11:59:57 -0400

    cardsJS added

---
+++ game/resources/views/play.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+@section('header')
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/cardsJS/dist/cards.min.css" />
+    <script src="https://unpkg.com/cardsJS/dist/cards.min.js" type="text/javascript"></script>
+@stop
+@section('content')
+
+@stop
