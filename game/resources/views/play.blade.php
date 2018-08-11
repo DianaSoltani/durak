ab6d61eeede8444da31b3459399816413ec1662f
DianaSoltani <diana.soltani@rutgers.edu>
2018-08-11 13:09:49 -0400

    Quick CardsJS set up

--- game/resources/views/play.blade.php
+++ game/resources/views/play.blade.php
@@ -1,10 +1,19 @@
 @extends('layout')
-@section('header')
-    <link rel="stylesheet" href="node_modules/deck-of-cards/example/example.css">
 
-@stop
 @section('content')
+    <script src="https://deck-of-cards.js.org/dist/deck.min.js"></script>
+    <link rel="stylesheet" href="https://deck-of-cards.js.org/example.css">
+    <div class="container">
+        <div id="container"></div>
+    </div>
+    <script>
+        var $container = document.getElementById('container')
 
-    
-    <script src="node_modules/deck-of-cards/dist/deck.min.js"></script>
+        // Instantiate a deck
+        var deck = Deck();
+
+        // display it in a html container
+        var $container = document.getElementById('container');
+        deck.mount($container);
+    </script>
 @stop
